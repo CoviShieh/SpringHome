93ba5d2b2cf79efa287ce7578a57ddc5befa2d07
covi <1530696130@qq.com>
2021-12-13 09:17:58 +0800

    springmvc注解版

---
+++ hellomvc-annotation/src/main/java/com/kuang/controller/helloController.java
@@ -0,0 +1,28 @@
+package com.kuang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author xiewh
+ * @create 2021-12-09 16:11
+ * @Description
+ */
+
+/*
+    @Controller最后会找到视图解析器再返回
+    @RestController只会返回字符串，适合返回json
+ */
+@Controller
+
+public class helloController {
+
+    @RequestMapping("/hi")
+    public String hello(Model model){
+
+        model.addAttribute("msg","this is my world!");
+
+        return "hello";
+    }
+}
